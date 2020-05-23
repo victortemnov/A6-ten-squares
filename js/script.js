5f1cee0389b62bf31aec17a3a25a0b3a6f83087a
ekke <63651605+victortemnov@users.noreply.github.com>
2020-05-23 14:02:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,72 +2,57 @@ const numDivs = 36;
 const maxHits = 10;
 
 let hits = 0;
-let fails = 0;
+let missHits = 0;
 let firstHitTime = 0;
 
-function getTimestamp() {
-  let d = new Date();
-  return d.getTime();
-}
-
-function randomDivId() {
-  let x = Math.floor(Math.random() * 6) + 1;
-  let y = Math.floor(Math.random() * 6) + 1;
-  return `#slot-${x}${y}`;
-}
-
 function round() {
-  $(".target").removeClass("target"); 
-  $(".miss").removeClass("miss"); 
+  $(".game-field").removeClass("target");
+  $(".game-field").html("");
 
-  let divSelect = randomDivId();
-  $(divSelect).addClass("target")
-  $(divSelect).text(hits + 1)
-  
-    if (hits === maxHits) {
-      $(divSelect).text(1);
-    }
-    if (hits === 1) { 
-      firstHitTime = getTimestamp();
-    } 
-    if (hits === maxHits) {
-      endGame();
-    } 
+  let divSelector = randomDivId();
+  $(divSelector).addClass("target");
+  $(divSelector).html(hits + 1);
+
+  if (hits === 0) {
+    firstHitTime = getTimestamp();
+  }
+
+  if (hits === maxHits) {
+    endGame();
+  }
 }
 
 function endGame() {
   $(".game-field").hide();
 
-  let playedMilliseconds = getTimestamp() - firstHitTime;
-  let playedSeconds = Number(playedMilliseconds / 1000).toPrecision(2);
-  $("#total-time-played").text(playedSeconds);
-  $("#total-scores").text(hits - fails);
-  $("#win-message").removeClass("hide-this");
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $("#total-time-played").text(totalPlayedSeconds);
+  $("#total-miss").text(hits - missHits);
+
+  $("#win-message").removeClass("d-none");
 }
 
 function handleClick(event) {
-  let target = $(event.target)
-  
-  if (target.hasClass("target")) {
-    hits += 1;
-    target.text("");
+  if ($(event.target).hasClass("target")) {
+    hits = hits + 1;
     round();
   } else {
-  	fails += 1
-  	$(event.target).addClass("miss"); 
-  } 
+    missHits = missHits + 1;
+    $(event.target).addClass("miss");
+  }
 }
 
 function init() {
-    round();
+  $(".game-field").click(handleClick);
 
-    $(".game-field").click(handleClick);
-    $("#button-start").click(function() {
-      hits = 0;
-      fails = 0;
-      firstHitTime = 0;
-      $(".game-field").show();
-      $("#win-message").addClass("hide-this");
+  $("#button-start").click(function() {
+    round();
+    $("#button-start").hide();
+  });
+
+  $("#button-reload").click(function() {
+    location.reload();
   });
 }
 
